d23722f2d1f93bb208c5452752282c739a7e9216
HHHHH-Y <984495875@qq.com>
2020-06-16 16:39:43 +0800

    线程中断

--- 多线程-基本操作/src/AboutThreadStop.java
+++ 多线程-基本操作/src/AboutThreadStop.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
- * Description:
+ * Description:中断一个线程
+ * 主线程中创建一个新的线程 (B)
+ * 主线程只负责通知让 (B) 停下来
+ *
+ * 如果子线程中有 sleep 方法, 那么为了可以让子线程接收到中断信息, interrupted 和 InterruptedException 都得写上
+ *
+ * 1. 如果对方正在 sleep / wait 等, 直接通知异常, 此时会将 isInterrupted 修改回 false
+ * 2. b.isInterrupted(), 这样的话, isInterrupted 会一直为 true
+ * 3. Thread.interrupted(), 这种情况下, isInterrupted 会先变成 true, 最后又修改成为 false
  * User: HHH.Y
  * Date: 2020-06-15
  */
 public class AboutThreadStop {
     static class B extends Thread {
-        B() {
+        // 通过调用一个构造方法, 给这个新线程取名为 B
+        B () {
             super("B");
         }
 
@@ -16,14 +25,17 @@ public class AboutThreadStop {
         public void run() {
             Thread t = Thread.currentThread();
             while (true) {
-                System.out.println("我正在追星");
+                System.out.println("我正在种太阳");
                 try {
                     Thread.sleep(1000);
                     boolean interrupted = t.isInterrupted();
-                    // 由于不知道 B 线程是否处于 sleep 状态, 所以两种情况都需要进行考虑
-                    System.out.println("是否有人让我停止: " + interrupted);
+                    System.out.println("是否有人让我停止运行: " + interrupted);
+                    if(interrupted == true) {
+                        // 干很多事
+                        break; // break, return 都可以
+                    }
                 } catch (InterruptedException e) {
-                    System.out.println("有人让我停止运行, 但因为我正在 sleep, 所以我收到了这个异常");
+                    System.out.println("有人让我停止运行, 但因为我正在 sleep, 所以我收到了这个异常!");
                 }
             }
         }
@@ -32,13 +44,12 @@ public class AboutThreadStop {
     public static void main(String[] args) {
         B b = new B();
         b.start();
-
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
-        System.out.println("准备通知B停止");
-        b.interrupt(); // 用于通知 B 停止
-        System.out.println("已经通知B停止");
 
-
+        System.out.println("准备通知 B 线程停止运行");
+        // 通知 B 线程停止
+        b.interrupt();
+        System.out.println("已经通知 B 线程停止运行");
     }
 }
--- 多线程-基本操作/src/AboutThreadStop2.java
+++ 多线程-基本操作/src/AboutThreadStop2.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description: 如何不使用 Thread 中设计的方式通知线程停止
+ *
+ * 对于这个程序而言:
+ * 主线程只负责通知 B 线程停止
+ * 对于 B 如何停止, 如何退出, 由 B 自行决定,
+ *
+ *
  * User: HHH.Y
  * Date: 2020-06-15
  */
 public class AboutThreadStop2 {
     // 定义了一个类变量, 线程之间可以共享
-    // B 线程可以通知观察该变量, 知道是否有人让它停止
-    private static volatile boolean isInterrupted = false;
-
+    // B 线程可以通过观察该变量, 知道是否有人让它停止
+    private static volatile boolean isInterrupted = false; // 初始化被中断变量为 false
     static class B extends Thread {
         B () {
             super("B");
@@ -16,8 +23,32 @@ public class AboutThreadStop2 {
 
         @Override
         public void run() {
-            System.out.println("我正在追星");
-
+            while (true) {
+                try {
+                    System.out.println("我正在种太阳");
+                    Thread.sleep(1000);
+                    System.out.println("是否有人让我停止: " + isInterrupted);
+                    if(isInterrupted) {
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    // 通过 InterruptedException 异常的方式通知有人让你停止, 这样的通知是非常及时的
+                    System.out.println("有人让我停止, 但由于我正在 sleep, 所以我收到了这个异常!");
+                    break;
+                }
+            }
         }
     }
+
+    public static void main(String[] args) {
+        B b  = new B();
+        b.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("准备通知 B 线程停止运行");
+        isInterrupted = true;
+        System.out.println("已经通知 B 线程停止运行");
+    }
+
 }
