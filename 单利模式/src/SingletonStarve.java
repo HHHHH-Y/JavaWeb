c1b8b09c2cc27630bc16b75fffc1f22bff1bed43
HHHHH-Y <984495875@qq.com>
2020-06-20 09:38:34 +0800

    饿汉模式的单利

--- 单利模式/src/SingletonStarve.java
+++ 单利模式/src/SingletonStarve.java
@@ -1,7 +1,7 @@
 /**
  * Created with IntelliJ IDEA.
  * Description: 饿汉模式的单利
- * 1. 保存仅有的一个对象至一个静态属性中
+ * 1. 保存仅有的一个引用至一个静态属性中
  * 2. 一开始就对这个对象进行初始化(new)(静态代码块 / 直接初始化)
  * 3. 提供一个方法可以返回这个对象
  * 4. 避免无意中犯错, 将这个类的构造方法标记成 private 的
@@ -11,7 +11,7 @@
 
 // 通过 final 修饰类, 避免有人定义该类的子类来构造对象(有时候)
 public final class SingletonStarve {
-    // 1. 我需要保存我仅有的一个对象(保存到一个静态属性中)
+    // 1. 我需要保存我仅有的一个引用(保存到一个静态属性中)
     private static final SingletonStarve instance;
 
     // 2. 饿汉模式, 也就是一开始就初始化
