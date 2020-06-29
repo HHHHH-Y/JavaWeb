14724bbc456e79b31d80e7e766754841c077389d
HHHHH-Y <984495875@qq.com>
2020-06-29 17:50:06 +0800

    UDP 下的 Socket 编程

--- Socket编程/src/udp/echo/Client.java
+++ Socket编程/src/udp/echo/Client.java
@@ -6,29 +6,31 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Date;
 import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
- * Description:
+ * Description: 客户端发送数据
  * User: HHH.Y
  * Date: 2020-06-28
  */
 public class Client {
+    // 需要明确服务端的 ip 地址
     public static final String SERVER_HOST = "127.0.0.1";
 
     public static void main(String[] args) {
         // 从标准输入中读取内容的 scanner
         Scanner stdinScanner = new Scanner(System.in);
 
-        // 创建一个 DatagramSocket，创建方式和 Server 的略有不同
-        // 明确明确我自己的 port，交给 OS 随意分配一个未被使用的 port 即可
-        // 我们是 Client，我们 地址 不需要公开
-        try (DatagramSocket clientSocket = new DatagramSocket()) {
+        // 创建一个 DatagramSocket, 创建方式和 Server 的略有不同
+        // 不需要明确我们自己的 port, 交给 OS 随意分配一个未使用的 port 即可
+        // 我们是 Client, 所以我们的地址不需要公开
+        try(DatagramSocket clientSocket = new DatagramSocket()) {
             System.out.printf("%s: DEBUG: 已经创建好 socket 对象%n", new Date());
             System.out.print("echo> ");
             while (stdinScanner.hasNextLine()) {
                 String request = stdinScanner.nextLine();
-                System.out.printf("%s: DEBUG: 读取用户的输入是: %s%n", new Date(), request);
-                if (request.equalsIgnoreCase("quit")) {
+                System.out.printf("%s, DEBUG: 读取用户的输入是: %s%n", new Date(), request);
+                if(request.equalsIgnoreCase("quit")) {
                     break;
                 }
 
@@ -36,13 +38,10 @@ public class Client {
                 byte[] sendBuffer = request.getBytes("UTF-8");
                 // 绑定对方的 ip
                 // 绑定对方的 port
-                // 127.0.0.1:8260
-                DatagramPacket sendPacket = new DatagramPacket(
-                        sendBuffer, sendBuffer.length,
-                        InetAddress.getByName(SERVER_HOST),
-                        Server.PORT);
+                // 127.0.0.1 : 9260
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, 0 , sendBuffer.length, InetAddress.getByName(SERVER_HOST), Server.PORT);
 
-                System.out.printf("%s: DEBUG: 准备发送请求 ...%n", new Date());
+                System.out.printf("%s: DEBUG: 准备发送响应 ... %n", new Date());
                 clientSocket.send(sendPacket);
                 System.out.printf("%s: DEBUG: 请求发送成功%n", new Date());
 
@@ -50,13 +49,13 @@ public class Client {
                 byte[] receiveBuffer = new byte[1024];
                 DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, 0, receiveBuffer.length);
 
-                System.out.printf("%s: DEBUG: 准备接收响应 ...%n", new Date());
+                System.out.printf("%s: DEBUG: 准备接收响应 ... %n", new Date());
                 clientSocket.receive(receivePacket);
-                System.out.printf("%s: DEBUG: 接收响应成功%n", new Date());
+                System.out.printf("%s: DEBUG: 响应接收成功 %n", new Date());
 
-                // 转成字符形式
+                // 转换成字符形式
                 String response = new String(receiveBuffer, 0, receivePacket.getLength(), "UTF-8");
-                System.out.println("echo service 应答: " + response);
+                System.out.println("echo server 应答: " + response);
                 System.out.print("echo> ");
             }
         } catch (IOException e) {
