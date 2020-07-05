940a0c1885992dbbfc10d4ce1572e7e92ebf3ef1
HHHHH-Y <984495875@qq.com>
2020-07-05 23:40:42 +0800

    HTTP 协议-动态内容练习

--- HTTP协议-动态内容练习/src/MyPracticeAboutHelloToday.java
+++ HTTP协议-动态内容练习/src/MyPracticeAboutHelloToday.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -6,5 +11,15 @@ package PACKAGE_NAME;
  * User: HHH.Y
  * Date: 2020-07-05
  */
-public class MyPracticeAboutHelloToday {
+public class MyPracticeAboutHelloToday extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String target = req.getParameter("target");
+
+        resp.setContentType("text/html; charset = utf-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.print("<h1>我是 /a 资源</h1>");
+        writer.println("<p>Hello Today</p>");
+    }
 }
--- HTTP协议-动态内容练习/src/MyPracticeAboutPxc.java
+++ HTTP协议-动态内容练习/src/MyPracticeAboutPxc.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -6,5 +11,16 @@ package PACKAGE_NAME;
  * User: HHH.Y
  * Date: 2020-07-05
  */
-public class MyPracticeAboutPxc {
+public class MyPracticeAboutPxc extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String age = req.getParameter("age");
+
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.print("<h1>我是 /bye 资源</h1>");
+        writer.println("<p> 再见" + name + ", 年龄 " + age + "</p>");
+    }
 }
